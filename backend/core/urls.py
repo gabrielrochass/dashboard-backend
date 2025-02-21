fa2313cfb2252a2b4ee0686d449e9e5b4717f839
gabrielrochass <grss@cin.ufpe.br>
2025-02-21 12:08:07 -0300

    feat: rota de metricas com cálculos básicos

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, EmployeeViewSet
+from .views import CompanyViewSet, EmployeeViewSet, MetricsView
 
 router = DefaultRouter()
 router.register(r'companies', CompanyViewSet)
@@ -10,4 +10,5 @@ router.register(r'employees', EmployeeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('metrics/', MetricsView.as_view(), name='metrics'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -3,6 +3,9 @@
 from rest_framework import viewsets
 from .models import Company, Employee
 from .serializers import CompanySerializer, EmployeeSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.db.models import Count
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
@@ -11,3 +14,25 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+ 
+class MetricsView(APIView):
+    
+    def get(self, request):
+        total_companies = Company.objects.count()
+        total_employees = Employee.objects.count()
+        avg_employees_per_company = total_employees / total_companies if total_companies > 0 else 0
+        max_employees_in_company = Employee.objects.values('company').annotate(count=Count('id')).order_by('-count').first()
+        max_employees_in_company = max_employees_in_company['count'] if max_employees_in_company else 0
+
+        min_employees_in_company = Employee.objects.values('company').annotate(count=Count('id')).order_by('count').first()
+        min_employees_in_company = min_employees_in_company['count'] if min_employees_in_company else 0
+
+        metrics = {
+            "total_companies": total_companies,
+            "total_employees": total_employees,
+            "avg_employees_per_company": avg_employees_per_company,
+            "max_employees_in_company": max_employees_in_company,
+            "min_employees_in_company": min_employees_in_company
+            
+        }
+        return Response(metrics)
